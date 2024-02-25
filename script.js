c741cd18972f4b6881eda626972980165ac3b6f7
ra397 <rabialaya@gmail.com>
2024-02-25 17:31:40 -0600

    Got basic functionality done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Define the URL of the API endpoint you want to call
+// api url endpoint
 const apiUrl = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
 
 // Make a GET request to the API endpoint using fetch
@@ -13,7 +13,27 @@ fetch(apiUrl)
     throw new Error('Network response was not ok.');
   })
   .then(data => {
-    console.log(data);
+    const table = document.getElementById("table");
+
+    for (let i = data['data'].length - 1; i >= 0; i --) {
+      // get data for each year
+      let year = data['data'][i]['Year'];
+      let population = data['data'][i]['Population'];
+      let formatted_population = population.toLocaleString('en-US');
+
+
+      // create and insert row for each year
+      const newRow = document.createElement('tr');
+      const yearCell = document.createElement('td');
+      const populationCell = document.createElement('td');
+
+      yearCell.textContent = year;
+      populationCell.textContent = formatted_population;
+
+      newRow.appendChild(yearCell);
+      newRow.appendChild(populationCell);
+      table.appendChild(newRow);
+    }    
   })
   .catch(error => {
     // Handle any errors that occur during the fetch operation
